be9fcfbaf55ab91a39f63caa6973966f9e1329d6
Maxym Levashov <55034578+dnopasta@users.noreply.github.com>
2024-02-10 17:52:27 +0200

    Add Unit awesome moving behaviour

--- Assets/Scripts/Gameplay/Entity/UnitControlBehaviour.cs
+++ Assets/Scripts/Gameplay/Entity/UnitControlBehaviour.cs
@@ -13,11 +13,14 @@ public class UnitControlBehaviour : MonoBehaviour
     public UnitBehaviour unitBehaviour;
 
     private Rigidbody2D rb;
+
+    private Vector2 positionToFollow;
     private StatAttribute Speed => unitBehaviour.GetStat("Speed");
     
     private void OnEnable()
     {
         rb = GetComponent<Rigidbody2D>();
+        
         InputManager.InputAction += InputAction;
     }
     
@@ -40,33 +43,46 @@ public class UnitControlBehaviour : MonoBehaviour
     private void UnitActionBehaviour(InputManager.PlayerAction action)
     {
         OnAction?.Invoke(action);
+
+        Vector2 possibleMoveVector = Vector2.zero;
         
-        switch (action)
+        if (InputManager.PlayerAction.bomb == action)
         {
-            case InputManager.PlayerAction.bomb:
-                DropBomb();
-                break;
-            case InputManager.PlayerAction.up:
-                MoveUnit(Vector2.up);
-                break;
-            case InputManager.PlayerAction.down:
-                MoveUnit(Vector2.down);
-                break;
-            case InputManager.PlayerAction.left:
-                MoveUnit(Vector2.left);
-                break;
-            case InputManager.PlayerAction.right:
-                MoveUnit(Vector2.right);
-                break;
+            DropBomb();
         }
+        if (InputManager.PlayerAction.down == action)
+        {
+            possibleMoveVector += Vector2.down;
+        }
+        if (InputManager.PlayerAction.left == action)
+        {
+            possibleMoveVector += Vector2.left;
+        }
+        if (InputManager.PlayerAction.up == action)
+        {
+            possibleMoveVector += Vector2.up;
+        }
+        if (InputManager.PlayerAction.right == action)
+        {
+            possibleMoveVector += Vector2.right;
+        }
+        
+        MoveUnit(possibleMoveVector);
     }
 
     private void MoveUnit(Vector2 direction)
     {
-        var position = (direction * 0.001f * Speed.Value) + rb.position;
-        Debug.Log(position);
-        rb.MovePosition(position);
+        if (direction != Vector2.zero)
+        {
+            Vector2 force = direction.normalized * Speed.Value;
+            rb.AddForce(force, ForceMode2D.Impulse);
+            float maxVelocity = 2f * Speed.Value;
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+            positionToFollow = rb.position;
+        }
     }
+
+
     
 
     private void DropBomb()
